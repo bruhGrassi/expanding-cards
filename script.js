9128f2eb54ad0ff65adadc7be49a08f2e9e2b3c4
bruhGrassi <brunagm88@gmail.com>
2024-05-15 10:31:19 -0300

    feat: add documentation

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ panels.forEach((panel) => {
   });
 });
 
+/**
+ * Function to remove the "active" class from all "panel" elements.
+ * This ensures that only one "panel" has the "active" class at a time.
+ */
 const removeActiveClasses = () => {
   panels.forEach((panel) => {
     panel.classList.remove("active");
